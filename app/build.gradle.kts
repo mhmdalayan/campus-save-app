b6faaaba6e89294b359d5770221eabc9b9ca96a1
AhmadAbouZeid <144588263+AhmadAbouZeid@users.noreply.github.com>
2025-12-25 12:47:23 +0200

    Added registration and login volley implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.volley)
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
